53b0ef56e59135dad2497c61a0bccec3a3ab6a87
James Childers <james.childers@gmail.com>
2023-03-02 17:19:48 -0600

    Initial commit

---
+++ config/initializers/generators.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.config.generators do |g|
+  g.helper(false)
+  g.test_framework(:rspec, fixture: false)
+
+  g.controller_specs(false)
+  g.helper_specs(false)
+  g.system_specs(false)
+  g.view_specs(false)
+end
